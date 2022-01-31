89b5357c6e632318f5095050a6dd42e28a1e5738
pzachraias1 <pzachraias1@my.hpu.edu>
2022-01-31 08:37:36 -1000

    adding a new directory called homework 3

---
+++ hw03/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cmath>
+#include "function.h"
+
+
+int main(int argc, const char * argv[]) {
+    
+    double angle = 0;
+    
+    while (angle <= 90){
+        printf("When the angle is %.2f --> %.2f meters \n",angle, range(angle));
+        angle ++;
+    }
+    
+}
